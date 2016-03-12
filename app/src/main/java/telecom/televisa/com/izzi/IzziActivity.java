ca0d0e9b68eddd733721f8bfd2df870881beafa3
Carlos <carlosvelezballesteros@gmail.com>
2016-03-11 21:45:24 -0600

    ahhhhhhh

---
+++ app/src/main/java/telecom/televisa/com/izzi/IzziActivity.java
@@ -0,0 +1,7 @@
+package telecom.televisa.com.izzi;
+
+/**
+ * Created by cevelez on 09/03/2016.
+ */
+public class IzziActivity {
+}
---
+++ app/src/main/java/televisa/telecom/com/controls/ExpField.java
@@ -0,0 +1,7 @@
+package televisa.telecom.com.controls;
+
+/**
+ * Created by cevelez on 10/03/2016.
+ */
+public class ExpField {
+}
