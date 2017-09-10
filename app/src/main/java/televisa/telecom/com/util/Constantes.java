eb2f13b94a7c16c91c6ea5981015055c20d6550f
Carlos <carlosvelezballesteros@gmail.com>
2017-09-09 19:25:21 -0500

    local

--- app/src/main/java/televisa/telecom/com/util/Constantes.java
+++ app/src/main/java/televisa/telecom/com/util/Constantes.java
@@ -10,9 +10,9 @@ public class Constantes {
 
 //wifi
   // public static String endpoint="http://172.21.31.103:8001/izziMovil/";
-   public static String endpoint="https://www.izzi.mx/izziMovil/";
+   //public static String endpoint="https://www.izzi.mx/izziMovil/";
   // public static String endpoint="http://172.21.20.88:8001/izziMovil/";
-  // public static String endpoint="http://10.25.11.223:8080/izziMovil/";
+   public static String endpoint="http://10.25.11.223:8080/izziMovil/";
     //public static String endpoint="http://172.21.40.139:7007/izziMovil/";
 //    public static String endpoint="http://172.21.20.88:8001/izziMovil/";
 }
